13f5da760dbaca73be4dcb73667b82fa69f638d1
unknown <amanagarwala4444@gmail.com>
2024-07-28 23:17:41 +0530

    final commit

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,9 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:["http://localhost:3000","https://project2-78r3.vercel.app"]
+}));
 
 app.use('/api/v1', transactionalRoutes);
 
